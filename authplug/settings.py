62cda99d67e04bc8d08bbd100514ab59a12e1656
Michael Elovskikh <wronglink@gmail.com>
2012-06-01 03:14:40 +0600

    Авторизация через HTTP заголовок Authorization
    * Добавил проверку на заголовок в мидлварю.
    * Добавил настройку названия схемы авторизации.
    * Тесты для авторизации через заголовки.
    * Добавил информацию об авторизации через заголовки в риадми.

--- authplug/settings.py
+++ authplug/settings.py
@@ -3,3 +3,5 @@ from django.conf import settings
 
 SIGNATURE_PARAMETER = getattr(settings, 'AUTHPLUG_SIGNATURE_PARAMETER', 'sign')
 CODE_PARAMETER = getattr(settings, 'AUTHPLUG_CODE_PARAMETER', 'code')
+
+AUTHORIZATION_SCHEME = getattr(settings, 'AUTHPLUG_AUTHORIZATION_SCHEME', 'AP')
